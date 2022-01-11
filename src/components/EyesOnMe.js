8822b5cdaa4c0f2ebd62ab4b728c1459fab19551
Daniel <dkilcullen25@gmail.com>
2022-01-11 16:52:18 -0600

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+function EyesOnMe() {
+    function handleFocus(e) {
+        console.log('Good!')
+    }
+
+    function handleBlur(r) {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return(
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
-// Code Keypad Component Here
+import React from "react"
+
+function Keypad() {
+    
+    function handleChange(e) {
+        // const str = e.target.value
+        console.log('Entering password...')
+    }
+    
+    return (
+        <React.Fragment>
+            <label>Password</label>
+            <input onChange={handleChange} name="pw" type="password" />
+        </React.Fragment>
+    )
+}
+
+export default Keypad
